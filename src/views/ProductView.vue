84a4811aeb256a5ae97d3f831790f7af187fcea0
Trishant Kumar <trishant.k@simformsolutions.com>
2023-08-23 12:21:53 +0530

    Adda axios and display products

--- src/views/ProductView.vue
+++ src/views/ProductView.vue
@@ -22,11 +22,11 @@
                 </tr>
             </thead>
             <tbody class="bg-white divide-y divide-gray-200 dark:bg-gray-800 dark:divide-gray-700">
-                <tr class="hover:bg-gray-100 dark:hover:bg-gray-700">
-                    <td class="py-4 px-6 text-sm font-medium text-gray-900 whitespace-nowrap dark:text-white">1</td>
-                    <td class="py-4 px-6 text-sm font-medium text-gray-900 whitespace-nowrap dark:text-white">Iphone</td>
-                    <td class="py-4 px-6 text-sm font-medium text-gray-500 whitespace-nowrap dark:text-white">Mobile Phone</td>
-                    <td class="py-4 px-6 text-sm font-medium text-gray-900 whitespace-nowrap dark:text-white">80,000</td>
+                <tr class="hover:bg-gray-100 dark:hover:bg-gray-700" v-for="(item, index) in allProducts.products" :key="index">
+                    <td class="py-4 px-6 text-sm font-medium text-gray-900 whitespace-nowrap dark:text-white">{{ index + 1 }}</td>
+                    <td class="py-4 px-6 text-sm font-medium text-gray-900 whitespace-nowrap dark:text-white">{{ item.title }}</td>
+                    <td class="py-4 px-6 text-sm font-medium text-gray-500 whitespace-nowrap dark:text-white">{{ item.category }}</td>
+                    <td class="py-4 px-6 text-sm font-medium text-gray-900 whitespace-nowrap dark:text-white">{{ item.price }}$</td>
                     <td class="py-4 px-6 text-sm font-medium text-right whitespace-nowrap">
                         <a href="#" class="text-blue-600 dark:text-blue-500 hover:underline">Edit</a>
                     </td>
@@ -35,4 +35,28 @@
         </table>
     </div>
 </template>
-<script></script>
+<script>
+import axios from 'axios';
+export default {
+    name: 'ProductView',
+    data() {
+        return {
+            allProducts: []
+        }
+    },
+    async mounted() {
+        await this.getProducts();
+    },
+    methods: {
+        getProducts() {
+            axios.get('https://dummyjson.com/products')
+            .then(res => {
+                this.allProducts = res.data;
+            })
+            .catch(err => {
+                console.log(err)
+            })
+        }
+    }
+}
+</script>
